bc4d93359d03c64be865504154c09b788c3b60a7
Youssef Sabry <youssef.sabry1200@gmail.com>
2024-08-26 21:20:09 +0300

    added search function

--- app/src/main/java/com/example/finalproject/network/APIInterface.kt
+++ app/src/main/java/com/example/finalproject/network/APIInterface.kt
@@ -1,8 +1,15 @@
 package com.example.finalproject.network
 
+import android.icu.text.StringSearch
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface APIInterface {
     @GET("api/json/v1/1/random.php")
     suspend fun getRandomProduct():Meal
+
+    @GET("api/json/v1/1/search.php")
+    suspend fun getMealBySearch(
+        @Query("s") search: String
+    ) : List<Meal>
 }
--- app/src/main/java/com/example/finalproject/repo/ApplicationRepoImpl.kt
+++ app/src/main/java/com/example/finalproject/repo/ApplicationRepoImpl.kt
@@ -2,8 +2,13 @@ package com.example.finalproject.repo
 
 import com.example.finalproject.models.User
 import com.example.finalproject.db.LocalDataSource
+import com.example.finalproject.network.Meal
+import com.example.finalproject.network.RemoteDataSource
 
-class ApplicationRepoImpl(private val localDataSource: LocalDataSource) : ApplicationRepository {
+class ApplicationRepoImpl(
+    private val localDataSource: LocalDataSource,
+    private val remoteDataSource: RemoteDataSource
+    ) : ApplicationRepository {
 
     override suspend fun getUserByEmail(email: String): User? {
         return localDataSource.getUserByEmail(email)
@@ -11,5 +16,9 @@ class ApplicationRepoImpl(private val localDataSource: LocalDataSource) : Applic
 
     override suspend fun insertNewUser(user: User) {
         localDataSource.insertNewUser(user)
+    }
+
+    override suspend fun getMealBySearch(search: String): List<Meal> {
+        return remoteDataSource.getMealBySearch(search)
     }
 }
