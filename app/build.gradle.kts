4c82d46b8d32c13cefb989d5d0f0f02c47272bfd
Hossam <eidhossam7@gmail.com>
2024-08-26 04:08:11 +0300

    Finished login UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //SDP Library
+    implementation(libs.sdp.android)
+
+    //ViewModel
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    //coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
 }
