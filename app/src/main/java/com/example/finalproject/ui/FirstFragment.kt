dc11f98d5e6ea285bd36f3a588e15f76766c0036
Youssef Sabry <youssef.sabry1200@gmail.com>
2024-08-26 20:39:15 +0300

    Changed column names

--- app/src/main/java/com/example/finalproject/ui/FirstFragment.kt
+++ app/src/main/java/com/example/finalproject/ui/FirstFragment.kt
@@ -3,6 +3,8 @@ package com.example.finalproject.ui
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
--- app/src/main/java/com/example/finalproject/ui/HomeActivity.kt
+++ app/src/main/java/com/example/finalproject/ui/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.finalproject.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,13 +15,13 @@ import com.example.finalproject.databinding.ActivityHomeBinding
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-     binding = ActivityHomeBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
@@ -29,14 +31,41 @@ private lateinit var binding: ActivityHomeBinding
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null)
-                    .setAnchorView(R.id.fab).show()
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-    val navController = findNavController(R.id.nav_host_fragment_content_home)
-    return navController.navigateUp(appBarConfiguration)
-            || super.onSupportNavigateUp()
+        val navController = findNavController(R.id.nav_host_fragment_content_home)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_sign_out -> {
+
+                return true
+            }
+            R.id.action_about_us -> {
+                val navController = findNavController(R.id.nav_host_fragment_content_home)
+                if(navController.currentDestination?.id != R.id.aboutUsFragment)
+                    if(navController.currentDestination?.id == R.id.FirstFragment)
+                        navController.navigate(R.id.action_FirstFragment_to_aboutUsFragment)
+
+                    else if(navController.currentDestination?.id == R.id.SecondFragment)
+                        navController.navigate(R.id.action_SecondFragment_to_aboutUsFragment)
+
+                return true
+            }
+        }
+        return false
+    }
+
 }
